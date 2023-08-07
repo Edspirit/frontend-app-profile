ba20a5f0c83324fb19708f7a98126348b3ec0420
milad-emami <milad.emami1025@gmail.com>
2023-08-07 16:45:00 +0330

    fix: update fa-IR translation

--- src/i18n/index.jsx
+++ src/i18n/index.jsx
@@ -1,5 +1,5 @@
 import arMessages from './messages/ar.json';
-import faIRMessages from './messages/fa.json';
+import faIRMessages from './messages/fa_IR.json';
 import frMessages from './messages/fr.json';
 import es419Messages from './messages/es_419.json';
 import zhcnMessages from './messages/zh_CN.json';
