8cd71494777036c429d33c9de02baad3c589ad59
milad-emami <milad.emami1025@gmail.com>
2023-10-31 12:16:56 +0330

    fix: fix favicon

--- src/head/Head.jsx
+++ src/head/Head.jsx
@@ -11,7 +11,6 @@ function Head({ intl }) {
       <title>
         {intl.formatMessage(messages['profile.page.title'], { siteName: getConfig().SITE_NAME })}
       </title>
-      <link rel="shortcut icon" href={getConfig().FAVICON_URL} type="image/x-icon" />
     </Helmet>
   );
 }
